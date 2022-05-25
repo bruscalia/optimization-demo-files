7277b3fb18f09274403b1c8b92d5e93269f16815
mooscalia project <93492480+mooscaliaproject@users.noreply.github.com>
2022-05-24 23:16:26 -0300

    updates on docs

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # optimization-demo-files
 This is a repository with examples and tutorials on numerical optimization.
 
-Check my [Medium profile](https://medium.com/@bruscalia12) for some interesting articles.
+Check my [Medium profile](https://medium.com/@bruscalia12) for some interesting articles about these examples.
+
+## Convex
+Here you will find:
+
+- Implementation from scratch of usual gradient-based line search algorithms in Python.
+- Solution of a constrained problem using the scipy.optimize minimize function.
+- Optimization of a chemical reactor problem.
+
+## Nonconvex
+Here you will find:
+
+- Implementation Differential Evolution using scipy.optimize.
+- Solutions of convex, nondifferentiable, and nonconvex problems using DE and classic algorithms.
 
 ## Contact
 bruscalia12@gmail.com
--- convex/unconstrained.py
+++ convex/unconstrained.py
@@ -8,23 +8,45 @@ class DescentAlgorithm:
     
     def __init__(self, fun, gradient=None, hess=None, nd={}, wolfe_c1=1e-4, wolfe_c2=0.1,
                  x_tol=1e-6, f_tol=1e-6, max_iter=50, save_history=False):
-        """White label solver for Descent Algorithms.
+        """
+        Solver for Gradient-based Descent Algorithms.
 
-        Args:
-            fun (callable): Objective function f(x, *args) of minimization problem.
-            gradient (callable or Nons, optional): Gradient of objective function gradient(x, *args)
-                of minimization problem. If None, numdifftools Gradient is created. Defaults to None.
-            hess (callable or HessianUpdateStrategy, optional): Hessian of the objective function.
-                In Newton method, should be provided as hess(x, *args), whereas in QuasiNewton methods
-                it should be a scipy.optimize.HessianUpdateStrategy instance, Defaults to None.
-            nd (dict, optional): Keyword arguments passed to numdifftools is gradient is not provided.
-                Defaults to {}.
-            wolfe_c1 (float, optional): Wolfe line search c1. Defaults to 1e-4.
-            wolfe_c2 (float, optional): Wolfe line search c2. Defaults to 0.1.
-            x_tol (float, optional): Tolerance for advance in x. Defaults to 1e-6.
-            f_tol (float, optional): Tolerance for advance in f(x). Defaults to 1e-6.
-            max_iter (int, optional): Max iterations. Defaults to 50.
-            save_history (bool, optional): Either of not to store history of iterations. Defaults to False.
+        Parameters
+        ----------
+        
+        fun : callable
+            Objective function ``f(x, *args)`` of minimization problem.
+            
+        gradient : callable or None, optional
+            Gradient of objective function ``gradient(x, *args)``
+            of minimization problem. If None, numdifftools Gradient is created. Defaults to None.
+            
+        hess : callable or HessianUpdateStrategy, optional
+            Hessian of the objective function.
+            In Newton method, should be provided as ``hess(x, *args)``, whereas in QuasiNewton methods
+            it should be a scipy.optimize.HessianUpdateStrategy instance, Defaults to None.
+            
+        nd : dict, optional
+            Keyword arguments passed to numdifftools is gradient is not provided.
+            Defaults to {}.
+            
+        wolfe_c1 : float, optional
+            Wolfe line search c1. Defaults to 1e-4.
+            
+        wolfe_c2 : float, optional
+            Wolfe line search c2. Defaults to 0.1.
+            
+        x_tol : float, optional
+            Tolerance for advance in x. Defaults to 1e-6.
+            
+        f_tol : float, optional
+            Tolerance for advance in f(x). Defaults to 1e-6.
+            
+        max_iter : int, optional
+            Max iterations. Defaults to 50.
+            
+        save_history : bool, optional
+            Either of not to store history of iterations. Defaults to False.
         """
         
         self.fun = fun
